9ca26bd444a183a59856cefb4551fdcf7e3707be
Nikhilgshetty082 <nikhil.23cs082@sode-edu.in>
2024-12-24 19:17:39 +0530

    Update PasswordGenerator2.java

--- PasswordGenerator2.java
+++ PasswordGenerator2.java
@@ -1,15 +1,6 @@
 import java.util.Random;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author smv
- */
 public class PasswordGenerator2 {
     public static void main(String[] arg){
         String upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
